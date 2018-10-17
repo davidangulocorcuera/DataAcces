b4cf9810405df31e4d63bc2d42bc4c958658fa50
David <davidangulocorcuera@gmail.com>
2018-10-17 10:14:13 +0200

    deleted some coments

--- src/HibernateModel.java
+++ src/HibernateModel.java
@@ -21,69 +21,6 @@ public class HibernateModel implements DataManager {
         session = util.getSessionFactory().openSession();
         System.out.println("conected");
     }
-	
-   /*
-	public void borrarDatos(){
-    	System.out.println("Inicio Borrado");
-
-		session.beginTransaction();
-    	
-		Query q = session.createQuery("delete from JugadorPosicion");
-		q.executeUpdate();
-		q = session.createQuery("delete from Posicion");
-		q.executeUpdate();
-		q = session.createQuery("delete from Jugador");
-		q.executeUpdate();
-		q = session.createQuery("delete from Equipo");
-		q.executeUpdate();
-		
-		session.getTransaction().commit();
-		System.out.println("Fin Borrado");
-	}
-	
-    public void consultaEquipo() {
-    	System.out.println("Inicio Consulta Simple Equipos");
-    	
-        Query q= session.createQuery("select e from Equipo e");
-        List results = q.list();
-        
-        Iterator equiposIterator= results.iterator();
-
-        while (equiposIterator.hasNext()){
-            Equipo team = (Equipo)equiposIterator.next();
-            
-        	System.out.println ( "		Id: " + team.getId() + " - Nombre: " + team.getNombre());
-            
-        }
-
-    	System.out.println("Fin Consulta Equipos");
-    }
-	
-	
-    public void consultaJugadorPosicion() {
-    	System.out.println("Inicio Consulta");
-    	
-        Query q= session.createQuery("select jp from JugadorPosicion jp order by jp.j.numero");
-        List results = q.list();
-        
-        Iterator jugadoresIterator= results.iterator();
-        int idJug;
-        int auxId = 0;
-        while (jugadoresIterator.hasNext()){
-            JugadorPosicion jp = (JugadorPosicion)jugadoresIterator.next();
-            Jugador j = jp.getJ();
-            idJug = j.getIdJugador();
-            if(idJug != auxId){
-            	System.out.println ( "(" + j.getNumero() + ") " + j.getNombre());
-            }
-        	System.out.println ( "		Posicion: " + jp.getP().getPosicion() + " - numero de Veces: " + jp.getNumVeces());
-        	auxId = idJug;
-            
-        }
-
-    	System.out.println("Fin Consulta");
-    } 
-*/
 
     public void cerrarSesion() {
 
