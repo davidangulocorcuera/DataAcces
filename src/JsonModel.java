f57baf39ba4e1db3ba4f964059888ca676048e15
davidangulocorcuera <davidangulocorcuera@gmail.com>
2018-12-01 14:41:19 +0100

    insert curses terminado

--- src/JsonModel.java
+++ src/JsonModel.java
@@ -11,7 +11,7 @@ public class JsonModel implements DataManager {
     private Curse newCurse;
     Curse curse;
     ApiRequests encargadoPeticiones;
-    private String SERVER_PATH, GET_ENTITY,GET_CURSE, SET_ENTITY; // Datos de la conexion
+    private String SERVER_PATH, GET_ENTITY,GET_CURSE, SET_ENTITY,SET_CURSE; // Datos de la conexion
 
     public JsonModel() {
         char c;
@@ -22,6 +22,7 @@ public class JsonModel implements DataManager {
         GET_ENTITY = "readEntity.php";
         GET_CURSE = "readCurse.php";
         SET_ENTITY = "writeEntity.php";
+        SET_CURSE = "writeCurse.php";
 
     }
 
@@ -106,8 +107,81 @@ public class JsonModel implements DataManager {
     }
 
     @Override
-    public void addCurse(Curse curse) throws FileNotFoundException, IOException {
+    public void addCurse(Curse auxCurse) throws FileNotFoundException, IOException {
+        try {
+            JSONObject objJCurse = new JSONObject();
+            JSONObject objPeticion = new JSONObject();
 
+            objJCurse.put("int_id", auxCurse.getInt_id());
+            objJCurse.put("str_mname", auxCurse.getStr_mname());
+            objJCurse.put("str_mfirst_characteristic", auxCurse.getStr_mfirst_characteristic());
+            objJCurse.put("str_msecond_characteristic", auxCurse.getStr_msecond_characteristic());
+            objJCurse.put("str_mthird_characteristic", auxCurse.getStr_mthird_characteristic());
+
+
+
+
+            // Tenemos el jugador como objeto JSON. Lo añadimos a una peticion
+            // Lo transformamos a string y llamamos al
+            // encargado de peticiones para que lo envie al PHP
+
+            objPeticion.put("peticion", "add");
+            objPeticion.put("curse", objJCurse);
+
+            String json = objPeticion.toJSONString();
+
+            System.out.println("Lanzamos peticion JSON para almacenar un jugador");
+
+            String url = SERVER_PATH + SET_CURSE;
+
+            System.out.println("La url a la que lanzamos la petición es " + url);
+            System.out.println("El json que enviamos es: ");
+            System.out.println(json);
+            //System.exit(-1);
+
+            String response = encargadoPeticiones.postRequest(url, json);
+
+            System.out.println("El json que recibimos es: ");
+
+            System.out.println(response); // Traza para pruebas
+            System.exit(-1);
+
+            // Parseamos la respuesta y la convertimos en un JSONObject
+
+
+            JSONObject respuesta = (JSONObject) JSONValue.parse(response.toString());
+
+            if (respuesta == null) { // Si hay algún error de parseo (json
+                // incorrecto porque hay algún caracter
+                // raro, etc.) la respuesta será null
+                System.out.println("El json recibido no es correcto. Finaliza la ejecución");
+                System.exit(-1);
+            } else { // El JSON recibido es correcto
+
+                // Sera "ok" si todo ha ido bien o "error" si hay algún problema
+                String estado = (String) respuesta.get("estado");
+                if (estado.equals("ok")) {
+
+                    System.out.println("Almacenado jugador enviado por JSON Remoto");
+
+                } else { // Hemos recibido el json pero en el estado se nos
+                    // indica que ha habido algún error
+
+                    System.out.println("Acceso JSON REMOTO - Error al almacenar los datos");
+                    System.out.println("Error: " + (String) respuesta.get("error"));
+                    System.out.println("Consulta: " + (String) respuesta.get("query"));
+
+                    System.exit(-1);
+
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(
+                    "Excepcion desconocida. Traza de error comentada en el método 'annadirJugador' de la clase JSON REMOTO");
+            // e.printStackTrace();
+            System.out.println("Fin ejecución");
+            System.exit(-1);
+        }
     }
 
     @Override
